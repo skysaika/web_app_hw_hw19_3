169594afe87498edaba8f5a4f13a185e9220cf9a
skysaika <skysaika@yandex.ru>
2023-06-06 13:59:12 +0300

    создан сервер webServer с портом 8080, выдает "Hello, World wide web!"

---
+++ 19/1_web_apps/19_1_task_3.py
@@ -0,0 +1,26 @@
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(bytes("Hello, World wide web!", "utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
